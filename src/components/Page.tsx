ee0abb3257400be7267e5d088da06c103a0bc48d
= <=>
2022-06-05 15:44:30 -0300

    page content

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Flex } from '@chakra-ui/react'
 import { ReactNode } from 'react'
 import { Header } from './Header'
 
@@ -10,6 +10,11 @@ export function Page({ children }: PageProps) {
   return (
     <Box>
       <Header />
+      <Flex w="100%" my="6" maxWidth={1480} px="6">
+        <Box flex="1" bg="gray.800" p="8" borderRadius="8">
+          {children}
+        </Box>
+      </Flex>
     </Box>
   )
 }

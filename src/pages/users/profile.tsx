6b3b6ed3bea0ae0d0821eb81df79b94a6ff3841e
= <=>
2022-06-05 20:51:41 -0300

    user profile

---
+++ src/pages/users/profile.tsx
@@ -0,0 +1,22 @@
+import { Button, Heading, Stack } from '@chakra-ui/react'
+import { Input } from '../../components/Form/Input'
+import { Page } from '../../components/Page'
+import { StackedButtonGroup } from '../../components/StackedButtonGroup'
+
+export default function Profile() {
+  return (
+    <Page>
+      <>
+        <Heading size="md">User Information</Heading>
+
+        <Stack mt="8">
+          <Input name="email" label="Email" type="email" />
+        </Stack>
+
+        <StackedButtonGroup>
+          <Button type="submit">Save</Button>
+        </StackedButtonGroup>
+      </>
+    </Page>
+  )
+}

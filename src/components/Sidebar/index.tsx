b493e032925535802b807754bc179741860eb282
= <=>
2022-06-05 20:41:41 -0300

    drawer menu

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,27 +1,36 @@
-import { Box, useBreakpointValue } from '@chakra-ui/react'
+import {
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+} from '@chakra-ui/react'
+import { useSidebarDrawer } from '../../contexts/SidebarDrawer'
+import { useWideVersion } from '../../hooks/useWideVersion'
 import { SidebarNav } from './SidebarNav'
 
 export function Sidebar() {
-  const isSidebarDrawer = useBreakpointValue({
-    base: true,
-    lg: false,
-  })
+  const isSidebarDrawer = !useWideVersion()
 
-  // if (isSidebarDrawer) {
-  //   return (
-  //     <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
-  //       <DrawerOverlay>
-  //         <DrawerContent bg="gray.800">
-  //           <DrawerCloseButton />
-  //           <DrawerHeader>Navegação</DrawerHeader>
-  //           <DrawerBody>
-  //             <SidebarNav />
-  //           </DrawerBody>
-  //         </DrawerContent>
-  //       </DrawerOverlay>
-  //     </Drawer>
-  //   )
-  // }
+  const { isOpen, onClose } = useSidebarDrawer()
+
+  if (isSidebarDrawer) {
+    return (
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerOverlay>
+          <DrawerContent bg="gray.800">
+            <DrawerCloseButton />
+            <DrawerHeader>Navegação</DrawerHeader>
+            <DrawerBody>
+              <SidebarNav />
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    )
+  }
 
   return (
     <Box as="aside" w="64" mr="8">
